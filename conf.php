ad6f45d8f180e7a625a464b2648b77c2cd2a062b
erkz555 <erikpahlberg45@gmail.com>
2017-03-15 15:29:14 +0200

    Conf

---
+++ conf.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erikp
+ * Date: 15.03.2017
+ * Time: 15:25
+ */
+define('CLASSES_DIR', 'classes/'); //clasess kataloogi konstant
+define('TMPL_DIR', 'tmpl/'); //clasess kataloogi konstant
+?>
