867d22a5bc9ccc4da93dcaed2c9129dbdf2affb3
erkz555 <erikpahlberg45@gmail.com>
2017-03-22 12:43:38 +0200

    Funktsioon selgitab faili olemasolu.

--- classes/template.php
+++ classes/template.php
@@ -13,6 +13,22 @@ class template
     var $content = false; //html malli faili sisu
     var $vars = array(); //html malli vaade sisu
 
+    function load_file(){
+        $f = $this->file;
+        if (!is_dir(TMPL_DIR)){
+            echo 'Kataloogi '.TMPL_DIR.' ei leitud.<br>';
+            exit;
+
+        }
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->read_file($f);
+        }
+        if ($this->content===false) {
+            echo 'Ei saanud sisu lugeda<br>';
+        }
+
+    }
+
     function read_file($f){
         $this->content = file_get_contents($f);
 
