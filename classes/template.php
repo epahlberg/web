9807f4f0430c147568a2ddcba58d6b2f40564255
erkz555 <erikpahlberg45@gmail.com>
2017-03-22 12:52:41 +0200

    Konstruktori loomine

--- classes/template.php
+++ classes/template.php
@@ -13,12 +13,21 @@ class template
     var $content = false; //html malli faili sisu
     var $vars = array(); //html malli vaade sisu
 
+    //klassi konstruktor
+
+    function __construct($f)
+    {
+        $this->file = $f; // määrame html malli nime
+        $this->load_file();
+
+    }
+
+    //html malli faili lugemine
     function load_file(){
         $f = $this->file;
         if (!is_dir(TMPL_DIR)){
             echo 'Kataloogi '.TMPL_DIR.' ei leitud.<br>';
             exit;
-
         }
         if(file_exists($f) and is_file($f) and is_readable($f)) {
             $this->read_file($f);
@@ -32,8 +41,8 @@ class template
     function read_file($f){
         $this->content = file_get_contents($f);
 
-
     }
+
 
 }
 
