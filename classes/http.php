1a3150e47b89c71ba542189e683dec88a6e07234
erkz555 <erikpahlberg45@gmail.com>
2017-03-29 18:45:14 +0300

    Utils

--- classes/http.php
+++ classes/http.php
@@ -12,19 +12,28 @@ class http
     var $vars = array(); // päringute andmed
     var $server = array(); //serveri andmed
     //algandmed:
+
+    function __construct()
+    {
+        $this->init();
+        $this->initCont();
+    }
     function init(){
         $this->vars = array_merge($_GET, $_POST, $_FILES);
         $this->server = $_SERVER;
     }
 
+
+
     //konstantide defineerimine
+
 
     function initCont()
     {
         $consts = array('REMOTE_ADDR', 'HTTP_HOST', 'PHP_SELF', 'SCRIPT_NAME');
         foreach($consts as $const){
-            if(!defined($consts) and isset($this->server[$const]));
-                define($consts, $this->server[$consts]);
+            if(!defined($const) and isset($this->server[$const]));
+                define($const, $this->server[$const]);
         }
     }
 
