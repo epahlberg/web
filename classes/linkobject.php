4e97d5048146055845b70d67bb34909ff6ffcea9
erkz555 <erikpahlberg45@gmail.com>
2017-03-29 18:46:32 +0300

    Uuendus

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -8,5 +8,39 @@
  */
 class linkobject extends http
 {
+    var $baseurl = false;
+    var $delimiter = '&amp';
+    var $eq = '=';
+    var $protocol = 'http://';
+
+    function __construct()
+    {
+        parent::__construct(); //http konstruktor tuleb tööle
+        $this->baseurl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }
+    //andmete paari lisamine kujul: asi=väärtus&asi=väärtus
+    function addtolink($link, $nimi, $val){
+        if($link == ''){
+            $link = $link.$this->delimiter;
+        }
+        $link = $link.fixurl($nimi).$this->eq.fixurl($val);
+        echo $link;
+
+    }
+    function getlink($add = array()){
+        $link = '';
+        foreach($add as $nimi=>$val){
+            $this->addtolink($link, $nimi, $val);
+        }
+        if ($link=''){
+            $link=$this->baseurl.'?'.$link;
+
+        }
+        else{
+            $link=$this->baseurl;
+        }
+        return $link;
+    }
+
 
 }
