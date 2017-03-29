d7ddbd3adf763a5b024c76bfc1ea612586cf004f
erkz555 <erikpahlberg45@gmail.com>
2017-03-29 18:45:30 +0300

    Utils

--- conf.php
+++ conf.php
@@ -8,14 +8,19 @@
 
 define('CLASSES_DIR', 'classes/'); //clasess kataloogi konstant
 define('TMPL_DIR', 'tmpl/'); //clasess kataloogi konstant
+define('LIB_DIR', 'lib/');
 
 //Failide kasutusele võtmine
 
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
 require_once CLASSES_DIR.'linkobject.php';
-$http = new linkobject();
+require_once LIB_DIR.'utils.php';
 
-echo REMOTE_ADDR;
+
+$http = new linkobject();
+echo $http->baseurl;
+echo '<br>';
+echo $link = $http->getlink(array('kasutaja'=>'nimi','pass'=>'parool'));
 
 ?>
