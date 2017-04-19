c8851229c3b2f66485273152a8aa100a971ac2d2
erkz555 <erikpahlberg45@gmail.com>
2017-04-19 09:49:51 +0300

    Parandused

--- classes/session.php
+++ classes/session.php
@@ -30,6 +30,19 @@ class session
             );
         }
 
+        $sid = md5(uniqid(time() .mt_rand(1, 1000), true));
+        $sql = 'INSERT INTO session SET '.
+            'sid='.fixDb($sid).', '.
+            'user_id='.fixDb($user['user_id']).', '.
+            'user_data='.fixDb(serialize($user)).', '.
+            'login_ip='.fixDb(REMOTE_ADDR).', '.
+            'created=NOW()';
+
+        $this->db->query($sql);
+        $this->sid=$sid;
+        $this->http->set('sid', $sid);
+
+
     }
 
 }
