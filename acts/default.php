6de260406164fb5232348266d1cae826a9aba0b8
erkz555 <erikpahlberg45@gmail.com>
2017-04-19 07:30:50 +0300

    Lehe sisu vaikimisi uuendamine

--- acts/default.php
+++ acts/default.php
@@ -6,8 +6,25 @@
  * Time: 9:18
  */
 
-$main_tmpl->set('content', 'Sisu');
 
+// lehe id saamine ja teisendamine - andmebaasis BIGINT
+$page_id = (int)$http->get('page_id');
+// sql lause lehe sisu otsimiseks vastavalt id-le
+$sql = 'SELECT * FROM content WHERE '.
+    'content_id='.fixDb($page_id);
+// saadame päring andmebaasi sisu saamiseks
+$res = $db->getArray($sql);
+if($res === false){
+    $sql = 'SELECT * FROM content WHERE '.
+        'is_first_page='.fixDb(1);
+    $res = $db->getArray($sql);
+}
+if($res !== false){
+    $page = $res[0];
+    $http->set('page_id', $page['content_id']);
+    $main_tmpl->set('content', $page['content']);
+}
+?>
 
 
 ?>
