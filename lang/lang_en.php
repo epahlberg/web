09c7f199bb0e79754af0a63867e42a5f60b42a35
erkz555 <erikpahlberg45@gmail.com>
2017-04-19 07:42:09 +0300

    Täiendatud keele tõlge

---
+++ lang/lang_en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erik
+ * Date: 4/19/17
+ * Time: 7:41 AM
+ */
+$_trans = array(
+    'Esimene leht' => 'First page',
+    'Teine leht' => 'Second page',
+    'avaleht' => 'main page',
+    'Lehe peamenüü' => 'Page menu',
+    'eesti' => 'estonian',
+    'inglise' => 'english',
+    'vene' => 'russian'
+);
+?>
