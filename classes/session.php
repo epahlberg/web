69dde4a911fbc69a976cf53cd2fc00cacf78365d
erkz555 <erikpahlberg45@gmail.com>
2017-04-19 09:22:16 +0300

    Parandused

---
+++ classes/session.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: erik
+ * Date: 4/19/17
+ * Time: 8:53 AM
+ */
+class session
+{
+    var $sid=false;
+    var $vars =array();
+    var $http = false;
+    var $db = false;
+    var $anonymus = true;
+
+    function __construct(&$http)
+    {
+        $this->http = &$http;
+        $this->db=&$db;
+
+    }
+
+}
